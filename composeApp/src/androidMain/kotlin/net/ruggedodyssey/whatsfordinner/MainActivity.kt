29a35ae3774c3f156db3365e80563593614bcc7d
Maia Grotepass <maiatoday@gmail.com>
2025-04-05 18:01:01 +0200

    Set light status bar appearance for MainActivity
    This update ensures the status bar uses a light appearance for better visibility, adapting to different Android versions. Devices running Android R or above use `WindowInsetsController`, while older versions fall back to `systemUiVisibility`.

--- composeApp/src/androidMain/kotlin/net/ruggedodyssey/whatsfordinner/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/net/ruggedodyssey/whatsfordinner/MainActivity.kt
@@ -1,6 +1,9 @@
 package net.ruggedodyssey.whatsfordinner
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -27,6 +30,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MainScreen()
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
     }
 
